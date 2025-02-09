c7e19603fe559286269db3b2b78dfb1be298c975
RedLily <RedLily@gmail.com>
2025-02-09 11:08:09 +0700

    fix config database host

--- config/db.js
+++ config/db.js
@@ -10,7 +10,7 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: process.env.DB_CONNECTION_LIMIT || 10,
   queueLimit: 0
 });
  
--- server.js
+++ server.js
@@ -4,8 +4,15 @@ const userRoutes = require("./routes/user");
 const itemRoutes = require("./routes/items");
 const requestRoutes = require("./routes/requests");
 const path = require("path");
- 
 const app = express();
+const fs = require('fs');
+const uploadDir = path.join(__dirname, 'uploads');
+
+// ตรวจสอบว่ามีโฟลเดอร์หรือยัง ถ้าไม่มีให้สร้างใหม่
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+    console.log('📁 Created uploads directory');
+}
 app.use(express.json());
 // ✅ เสิร์ฟโฟลเดอร์ `uploads/` ให้สามารถเข้าถึงรูปภาพที่อัปโหลดได้
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
